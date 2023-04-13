a887fdd33fbec8050b2e48aa831825a940d15ddb
ducdat2004 <ducdat05112004@gmail.com>
2023-04-13 08:40:05 +0700

    s

---
+++ dslk.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+	float diem;
+	node *tiep;
+};
+
+int main(){
+	do{
+		node *q=new node;
+		cin>>q->diem;
+		q->tiep=NULL;
+	}while(node->diem != -1 );
+
+}
